36d6d61492e4860e158aa3485661329148647d5a
Phoebe Torchia <phoebetorchia@Phoebes-Air.fios-router.home>
2021-07-01 12:08:19 -0400

    updated App Template

--- client/store/index.js
+++ client/store/index.js
@@ -2,13 +2,12 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
+import reduce from './reducer'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({reduce})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
 
 export default store
-export * from './auth'
