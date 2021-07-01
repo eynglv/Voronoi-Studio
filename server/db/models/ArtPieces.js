f3ddc7e14a49b6afeca8db25d11ab930a26516b1
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-01 16:22:28 -0400

    bug: fixed naming in db and server stuff

--- server/db/models/ArtPieces.js
+++ server/db/models/ArtPieces.js
@@ -1,45 +1,45 @@
 const Sequelize = require("sequelize");
-const db = require("./database");
+const db = require("../db");
 
 module.exports = db.define("artPieces", {
-  objectID: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  title: {
-    type: Sequelize.STRING,
-  },
-  artistDisplayName: {
-    type: Sequelize.STRING,
-  },
-  artistGender: {
-    type: Sequelize.STRING,
-  },
-  endDate: {
-    type: Sequelize.INTEGER,
-  },
-  primaryImage: {
-    type: Sequelize.STRING,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  primaryImageSmall: {
-    type: Sequelize.STRING,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  culture: {
-    type: Sequelize.STRING,
-  },
-  country: {
-    type: Sequelize.STRING,
-  },
-  isHighlighted: {
-    type: Sequelize.DataTypes.BOOLEAN,
-  },
+	objectID: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		validate: {
+			notEmpty: true,
+		},
+	},
+	title: {
+		type: Sequelize.STRING,
+	},
+	artistDisplayName: {
+		type: Sequelize.STRING,
+	},
+	artistGender: {
+		type: Sequelize.STRING,
+	},
+	endDate: {
+		type: Sequelize.INTEGER,
+	},
+	primaryImage: {
+		type: Sequelize.STRING,
+		validate: {
+			notEmpty: true,
+		},
+	},
+	primaryImageSmall: {
+		type: Sequelize.STRING,
+		validate: {
+			notEmpty: true,
+		},
+	},
+	culture: {
+		type: Sequelize.STRING,
+	},
+	country: {
+		type: Sequelize.STRING,
+	},
+	isHighlighted: {
+		type: Sequelize.DataTypes.BOOLEAN,
+	},
 });
