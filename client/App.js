9affb1b566a0c67a22cc8a3923b88eeeef5b9ab3
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-01 16:23:07 -0400

    working prototype

--- client/App.js
+++ client/App.js
@@ -1,26 +1,24 @@
-import React from 'react'
-
-import Navbar from './components/Navbar'
-import Routes from './Routes'
-import * as d3 from 'd3'
-import { Delaunay } from 'd3-delaunay'
+import React from "react";
+import container from "./components/vornoi";
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
+// import { d3 } from "d3";
+// import { Delaunay } from "d3-delaunay";
+// import { Delaunay } from "https://cdn.skypack.dev/d3-delaunay@6%22";
 
 class App extends React.Component {
-  componentDidMount() {
-    const square = d3.selectAll("rect");
-    square.style("fill", "orange");
-
-  }
-  render() {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
-  }
+	componentDidMount() {
+		container.render("#svg1", 300);
+		container.render("#svg2", 10);
+	}
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<Routes />
+			</div>
+		);
+	}
 }
 
-
-
-export default App
+export default App;
---
+++ client/components/vornoi.js
@@ -0,0 +1,92 @@
+// const d3=require('d3')
+const container = {};
+container.render = (selector, cellCount) => {
+	var svg = d3.select(selector), //selects element tagged svg
+		width = +svg.attr("width"), //accessing the width property
+		height = +svg.attr("height"), //accessing the height property
+		radius = 10;
+	//range sets the number of circles
+	const circles = d3.range(cellCount).map(function () {
+		return {
+			x: Math.round(Math.random() * (width - radius * 2) + radius),
+			y: Math.round(Math.random() * (height - radius * 2) + radius),
+		};
+	});
+	const color = d3.scaleOrdinal().range(d3.schemeCategory20);
+	const voronoi = d3
+		.voronoi()
+		.x(function (d) {
+			return d.x;
+		})
+		.y(function (d) {
+			return d.y;
+		})
+		.extent([
+			[-1, -1],
+			[width + 1, height + 1],
+		]);
+	//selects all the children of SVG's containers
+	const circle = svg
+		.selectAll("g")
+		.data(circles)
+		.enter()
+		.append("g")
+		.call(
+			d3
+				.drag()
+				.on("start", dragstarted)
+				.on("drag", dragged)
+				.on("end", dragended)
+		);
+	let cell = circle
+		.append("path")
+		.data(voronoi.polygons(circles))
+		.attr("d", renderCell)
+		.attr("id", function (d, i) {
+			return "cell-" + i;
+		});
+	circle
+		.append("clipPath")
+		.attr("id", function (d, i) {
+			return "clip-" + i;
+		})
+		.append("use")
+		.attr("xlink:href", function (d, i) {
+			return "#cell-" + i;
+		})
+		.style("fill", function (d, i) {
+			return color(i);
+		});
+	circle
+		.append("circle")
+		.attr("clip-path", function (d, i) {
+			return "url(#clip-" + i + ")";
+		})
+		.attr("cx", function (d) {
+			return d.x;
+		})
+		.attr("cy", function (d) {
+			return d.y;
+		})
+		.attr("r", radius)
+		.style("fill", function (d, i) {
+			return color(i);
+		});
+	function dragstarted(d) {
+		d3.select(this).raise().classed("active", true);
+	}
+	function dragged(d) {
+		d3.select(this)
+			.select("circle")
+			.attr("cx", (d.x = d3.event.x))
+			.attr("cy", (d.y = d3.event.y));
+		cell = cell.data(voronoi.polygons(circles)).attr("d", renderCell);
+	}
+	function dragended(d, i) {
+		d3.select(this).classed("active", false);
+	}
+	function renderCell(d) {
+		return d == null ? null : "M" + d.join("L") + "Z";
+	}
+};
+export default container;
