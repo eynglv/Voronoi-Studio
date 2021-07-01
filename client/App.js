d260a0c602a20b63bb8af84859702d4e77a41cda
Elvy Yang <hyang6@wellesley.edu>
2021-07-02 04:28:03 +0800

    comment out d3 import

--- client/App.js
+++ client/App.js
@@ -1,26 +1,23 @@
-import React from 'react'
+import React from "react";
 
-import Navbar from './components/Navbar'
-import Routes from './Routes'
-import * as d3 from 'd3'
-import { Delaunay } from 'd3-delaunay'
+import Navbar from "./components/Navbar";
+import Routes from "./Routes";
+// import * as d3 from 'd3'
+// import { Delaunay } from 'd3-delaunay'
 
 class App extends React.Component {
   componentDidMount() {
     const square = d3.selectAll("rect");
     square.style("fill", "orange");
-
   }
   render() {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+    return (
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    );
   }
 }
 
-
-
-export default App
+export default App;
