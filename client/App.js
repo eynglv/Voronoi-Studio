6f31a89e7b8cea22cd79a708855b76602b32dde6
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-07-06 17:02:49 -0400

    feat: display static voronoi with canvas

--- client/App.js
+++ client/App.js
@@ -2,24 +2,53 @@ import React from "react";
 import container from "./components/vornoi";
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
+
 import chart from "./components/pulsate";
+
+// import * as Scroll from 'react-scroll';
+import {animateScroll as scroll} from 'react-scroll';
 // import { d3 } from "d3";
 // import { Delaunay } from "d3-delaunay";
 // import { Delaunay } from "https://cdn.skypack.dev/d3-delaunay@6%22";
 
 class App extends React.Component {
-  componentDidMount() {
-    container.render("#svg1", 13);
-    // chart.render("#container", 200, 950, 200);
+	constructor(){
+		super();
+		this.scrollToTop = this.scrollToTop.bind(this);
+	}
+
+	componentDidMount() {
+		// container.render("#svg1", 10);
+		// container.render("#svg2", 20);
+		// container.render("#svg3", 15);
+		// chart.generator("#canvas1", 500, 960, 30)
+		chart.render("#canvas1", 500, 960, 30)
+	}
+
+	scrollToTop () {
+    scroll.scrollToTop();
   }
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Routes />
-      </div>
-    );
-  }
+
+	render() {
+		return (
+			<div>
+				<Navbar />
+				<Routes />
+				<canvas id="canvas1"width="960" height="500"></canvas>
+
+				<svg id="svg1" width="960" height="500"></svg>
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+				</p>
+				<svg id="svg2" width="960" height="500"></svg>
+				<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
+				</p>
+				<svg id="svg3" width="960" height="500"></svg>
+				<br/>
+				<a onClick={() => this.scrollToTop()}>To the top!</a>
+			</div>
+		);
+	}
+
 }
 
 export default App;
--- client/components/pulsate.js
+++ client/components/pulsate.js
@@ -1,6 +1,8 @@
 const chart = {};
-chart.render = (selector, height, width, cellCount) => {
-  const canvas = d3.select(selector).append("canvas");
+// const chart = { *generator(selector, height, width, cellCount){
+  chart.render = (selector, height, width, cellCount) => {
+  const canvas = d3.select(selector);
+  console.log(canvas);
   const n = 200;
   const context = canvas.node().getContext("2d");
   const positions = Float64Array.from(
@@ -8,36 +10,40 @@ chart.render = (selector, height, width, cellCount) => {
     (_, i) => Math.random() * (i & 1 ? height : width)
   );
   const velocities = new Float64Array(n * 2);
-  //   const voronoi = new d3.Delaunay(positions).voronoi([
-  //     0.5,
-  //     0.5,
-  //     width - 0.5,
-  //     height - 0.5,
-  //   ]);
+    const voronoi = new d3.Delaunay(positions).voronoi([
+      0.5,
+      0.5,
+      width - 0.5,
+      height - 0.5,
+    ]);
+    let j = 0
+    while (j < 1000000000) {
+  context.fillStyle = "#ffffff33";
+  context.fillRect(0, 0, width, height);
+  context.fillStyle = "black";
 
-  //   while (true) {
-  // context.fillStyle = "#ffffff33";
-  // context.fillRect(0, 0, width, height);
-  // context.fillStyle = "black";
+    for (let i = 0; i < positions.length; ++i) {
+      const size = i & 1 ? height : width;
+      positions[i] += velocities[i];
+      if (positions[i] < 0) positions[i] += size;
+      else if (positions[i] > size) positions[i] -= size;
+      velocities[i] += 0.2 * (Math.random() - 0.5) - 0.01 * velocities[i];
+    }
 
-  //   for (let i = 0; i < positions.length; ++i) {
-  //     const size = i & 1 ? height : width;
-  //     positions[i] += velocities[i];
-  //     if (positions[i] < 0) positions[i] += size;
-  //     else if (positions[i] > size) positions[i] -= size;
-  //     velocities[i] += 0.2 * (Math.random() - 0.5) - 0.01 * velocities[i];
-  //   }
+  context.beginPath();
+  voronoi.update().render(context);
+  voronoi.renderBounds(context);
+  context.stroke();
 
-  // context.beginPath();
-  // voronoi.update().render(context);
-  // voronoi.renderBounds(context);
-  // context.stroke();
+  context.beginPath();
+  voronoi.delaunay.renderPoints(context, 1);
+  context.fill();
+  j++;
 
-  // context.beginPath();
-  // voronoi.delaunay.renderPoints(context, 1);
-  // context.fill();
-
+ return;
   // yield context.canvas;
-  //   }
-};
+    }
+}
+// }
+;
 export default chart;
