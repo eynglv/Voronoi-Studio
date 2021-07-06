02a21f3bbf70082f7ecf48ff8c316ec11c59feaa
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-06 12:29:05 -0400

    bug: fixed filepath of art data

--- client/components/vornoi.js
+++ client/components/vornoi.js
@@ -1,6 +1,5 @@
 // const d3=require('d3')
-import womenByWomen from "../../womenByWomen";
-
+import womenByWomen from "../../script/artdata/womenByWomen";
 
 const container = {};
 container.render = (selector, cellCount) => {
@@ -51,42 +50,29 @@ container.render = (selector, cellCount) => {
 			.attr("width", "100%") //in pixels//could this be changed to the width of the cell.
 			.attr("height", "100%") //in pixels
 			.attr("patternUnits", "objectBoundingBox")
+			.attr("patternContentUnits", "objectBoundingBox")
 			// .style("fill-rule","evenodd")
 			.append("svg:image")
 			.attr("href", painting.primaryImageSmall)
-			.attr("preserveAspectRatio", "xMinYMid slice")
-			.attr("width", "100%")
-			.attr("height", "100%");
+			.attr("preserveAspectRatio", "xMidYMid slice")
+			.attr("width", "75%")
+			.attr("height", "75%");
 	});
+	const color = d3.scaleOrdinal().range(d3.schemeCategory20);
 
-  const color = d3.scaleOrdinal().range(d3.schemeCategory20);
-  function repeat(d) {
-    console.log(this)
-    d3.select(this).raise().classed("active", true)
-    d3.select(this).transition().duration(2000).delay(10000).attr("x", (d.x = d.x + 200)).attr("y", (d.y = d.y + 100))
-    d3.select(this).selectAll("path")
-      .transition()
-      .delay(10000)
-      .duration(5000)
-      .attr("x", (d.x = d.x +100))
-      .attr("y", (d.y = d.y +100))
-   }
-
-  //selects all the children of SVG
-  const circle = svg
-    .selectAll("g")
-    .data(circles)
-    .enter()
-    .append("g")
-    .each(repeat)
-    
-  circle.call(
-      d3
-        .drag()
-        .on("start", dragstarted)
-        .on("drag", dragged)
-        .on("end", dragended)
-    )
+	//selects all the children of SVG's containers
+	const circle = svg
+		.selectAll("g")
+		.data(circles)
+		.enter()
+		.append("g")
+		.call(
+			d3
+				.drag()
+				.on("start", dragstarted)
+				.on("drag", dragged)
+				.on("end", dragended)
+		);
 
 	let cell = circle
 		.append("path")
@@ -97,62 +83,52 @@ container.render = (selector, cellCount) => {
 		})
 		.style("fill", (d, i) => `url(#painting${i % womenByWomen.length}`);
 
-    circle.append("clipPath")
-    .attr("id", function (d, i) {
-      return "clip-" + i;
-    })
-    
-    .append("use")
-    .attr("xlink:href", function (d, i) {
-      return "#cell-" + i;
-    })
-    .style("fill", function (d, i) {
-      return color(i);
-    });
+	circle
+		.append("clipPath")
+		.attr("id", function (d, i) {
+			return "clip-" + i;
+		})
+		.append("use")
+		.attr("xlink:href", function (d, i) {
+			return "#cell-" + i;
+		})
+		.style("fill", function (d, i) {
+			return color(i);
+		});
 
-  circle
-    .append("circle")
-    .attr("class", function (d, i) {
-      return "cell-" + i;
-    })
-    .attr("clip-path", function (d, i) {
-      return "url(#clip-" + i + ")";
-    })
-    .attr("cx", function (d) {
-      return d.x;
-    })
-    .attr("cy", function (d) {
-      return d.y;
-    })
-    .attr("r", radius)
-    .style("fill", function (d, i) {
-      return color(i);
-    })
-
-
-
+	circle
+		.append("circle")
+		.attr("clip-path", function (d, i) {
+			return "url(#clip-" + i + ")";
+		})
+		.attr("cx", function (d) {
+			return d.x;
+		})
+		.attr("cy", function (d) {
+			return d.y;
+		})
+		.attr("r", radius)
+		.style("fill", function (d, i) {
+			return color(i);
+		});
 
 	function dragstarted(d) {
 		d3.select(this).raise().classed("active", true);
 	}
 
-  function dragged(d) {
-    console.log(this)
-    d3.select(this)
-      .selectAll("path")
-      .attr("cx", (d.x = d3.event.x +100))
-      .attr("cy", (d.y = d3.event.y +10));
-    cell = cell.data(voronoi.polygons(circles)).attr("d", renderCell);
-  }
-
-  function dragended(d, i) {
-    d3.select(this).classed("active", false);
-  }
-
-  function renderCell(d) {
-    const dPath = d == null ? null : "M" + d.join("L") + "Z";
-    return dPath;
-  }
-}
-
+	function dragged(d) {
+		d3.select(this)
+			.select("circle")
+			.attr("cx", (d.x = d3.event.x))
+			.attr("cy", (d.y = d3.event.y));
+		cell = cell.data(voronoi.polygons(circles)).attr("d", renderCell);
+	}
+	function dragended(d, i) {
+		d3.select(this).classed("active", false);
+	}
+	function renderCell(d) {
+		const dPath = d == null ? null : "M" + d.join("L") + "Z";
+		return dPath;
+	}
+};
 export default container;
