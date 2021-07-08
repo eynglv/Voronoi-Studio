b086aef69b05184ec1977efb0bb5f75321d8b3bb
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-07 23:17:03 -0400

    refactor: added explanatory comments

--- client/components/pulsate.js
+++ client/components/pulsate.js
@@ -14,18 +14,20 @@ const chart = {
 			};
 		});
 		const positions = Float64Array.from(
-			{ length: cellCount * 2 },
-			(_, i) => Math.random() * (i & 1 ? height : width)
+			{ length: cellCount * 2 }, //create an array of alternating x and y coordinates
+			(_, i) => Math.random() * (i & 1 ? height : width) //that are between 0 and heigh and width, respectively
 		);
-		const velocities = new Float64Array(cellCount * 2);
+		const velocities = new Float64Array(cellCount * 2); //create an array of alternating x and y velocities
 		const voronoi = new d3.Delaunay(positions).voronoi([
 			0.5,
 			0.5,
 			width - 0.5,
 			height - 0.5,
-		]);
+		]); //create a new voronoi from our positions array, with infinite polygons clipped at the provided minimums and maximums
 
 		while (true) {
+			//we will yield control back to the caller at the end of this loop, so it isn't actually infinite
+
 			//below block creates semitransparent rectangle over entire canvas
 
 			// context.fillStyle = "ffffff30";
@@ -33,19 +35,22 @@ const chart = {
 			// context.fillStyle = "white";
 
 			for (let i = 0; i < positions.length; ++i) {
-				const size = i & 1 ? height : width;
-				positions[i] += velocities[i];
+				//change every position
+				const size = i & 1 ? height : width; //alternate between height and width
+				positions[i] += velocities[i]; //change the position based on corresponding velocity
 				if (positions[i] < 0) positions[i] += size;
-				else if (positions[i] > size) positions[i] -= size;
+				//if position is less than 0, wrap around to the other side
+				else if (positions[i] > size) positions[i] -= size; //if position is greater than the canvas, wrap around to the other side
 				velocities[i] +=
-					0.2 * (Math.random() - 0.5) - 0.01 * velocities[i];
+					0.2 * (Math.random() - 0.5) - 0.01 * velocities[i]; //change the velocity by a random amount, with some consideration for it's previous value
 			}
-			voronoi.update();
+			voronoi.update(); //update the voronoi diagram with the new positions
 			for (let i = 0; i < positions.length / 2; i++) {
-				context.fillStyle = patterns[i % patterns.length];
-				context.beginPath();
-				voronoi.renderCell(i, context);
-				context.fill();
+				//for each cell
+				context.fillStyle = patterns[i % patterns.length]; //choose the next pattern in the array, looping back to the beginning if we go out of bounds
+				context.beginPath(); //start a new path
+				voronoi.renderCell(i, context); //trace the path of the current cell
+				context.fill(); //fill the cell withour chosen pattern
 			}
 			//below block renders lines between cells
 
@@ -60,7 +65,7 @@ const chart = {
 			// voronoi.delaunay.renderPoints(context, 1);
 			// context.fill();
 
-			yield context.canvas;
+			yield context.canvas; //return to caller with the canvas
 		}
 	},
 };
