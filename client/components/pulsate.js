7debaddff99a3768c8619817b807c8fac53ab312
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-07 23:31:11 -0400

    refactor: reduced equation for when position>size

--- client/components/pulsate.js
+++ client/components/pulsate.js
@@ -50,7 +50,7 @@ const chart = {
 				//below code block causes cells to bounce off of edges
 				if (positions[i] < 0) positions[i] *= -1;
 				else if (positions[i] > size)
-					positions[i] = size - (positions[i] - size);
+					positions[i] = 2 * size - positions[i];
 
 				velocities[i] +=
 					0.2 * (Math.random() - 0.5) - 0.01 * velocities[i]; //change the velocity by a random amount, with some consideration for it's previous value
