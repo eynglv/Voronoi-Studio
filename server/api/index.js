9f80a734832f4d6e9ebfc3de4049f24e7d9e37f3
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-07-09 15:51:47 -0400

    feat: voronois created by API calls

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 module.exports = router;
 
 router.use("/paintings", require("./paintings"));
+router.use("/voronois", require("./voronois"));
 
 router.use((req, res, next) => {
 	const error = new Error("Not Found");
