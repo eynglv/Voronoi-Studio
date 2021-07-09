4fb250d152f9e46e561ee4e9a5f0ab52d96d4894
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-09 13:11:44 -0400

    feat: added nonstateful dropdown departments menu

--- client/components/VoronoiForm.js
+++ client/components/VoronoiForm.js
@@ -9,7 +9,7 @@ import axios from "axios";
 export default () => {
 	const [highlight, setHighlight] = useState(true);
 	const [tags, setTags] = useState(true);
-	const [departmentId, setDepartmentId] = useState(null);
+	const [departmentId, setDepartmentId] = useState(0);
 	const [location, setLocation] = useState(null);
 	const [artistOrCulture, setArtistOrCulture] = useState(false);
 	const [query, setQuery] = useState("");
@@ -47,6 +47,96 @@ export default () => {
 				/>
 				<label htmlFor="tags">Tags</label>
 			</div>
+			<label htmlFor="department">Filter to Department</label>
+			<select name="department" id="department">
+				<option value="0">Any</option>
+				{[
+					{
+						departmentId: 1,
+						displayName: "American Decorative Arts",
+					},
+					{
+						departmentId: 3,
+						displayName: "Ancient Near Eastern Art",
+					},
+					{
+						departmentId: 4,
+						displayName: "Arms and Armor",
+					},
+					{
+						departmentId: 5,
+						displayName:
+							"Arts of Africa, Oceania, and the Americas",
+					},
+					{
+						departmentId: 6,
+						displayName: "Asian Art",
+					},
+					{
+						departmentId: 7,
+						displayName: "The Cloisters",
+					},
+					{
+						departmentId: 8,
+						displayName: "The Costume Institute",
+					},
+					{
+						departmentId: 9,
+						displayName: "Drawings and Prints",
+					},
+					{
+						departmentId: 10,
+						displayName: "Egyptian Art",
+					},
+					{
+						departmentId: 11,
+						displayName: "European Paintings",
+					},
+					{
+						departmentId: 12,
+						displayName: "European Sculpture and Decorative Arts",
+					},
+					{
+						departmentId: 13,
+						displayName: "Greek and Roman Art",
+					},
+					{
+						departmentId: 14,
+						displayName: "Islamic Art",
+					},
+					{
+						departmentId: 15,
+						displayName: "The Robert Lehman Collection",
+					},
+					{
+						departmentId: 16,
+						displayName: "The Libraries",
+					},
+					{
+						departmentId: 17,
+						displayName: "Medieval Art",
+					},
+					{
+						departmentId: 18,
+						displayName: "Musical Instruments",
+					},
+					{
+						departmentId: 19,
+						displayName: "Photographs",
+					},
+					{
+						departmentId: 21,
+						displayName: "Modern Art",
+					},
+				].map((val) => (
+					<option
+						value={`${val.departmentId}`}
+						key={val.departmentId}
+					>
+						{val.displayName}
+					</option>
+				))}
+			</select>
 		</form>
 	);
 };
