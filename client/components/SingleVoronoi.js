2dae894a8f70ffc8193ebe72e215c075ed35bfcb
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-13 15:21:26 -0400

    basic rendering of a single voronoi

---
+++ client/components/SingleVoronoi.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/display-name */
+import React, { useEffect, useState } from "react";
+import chart from "./pulsate";
+import axios from "axios";
+
+export default (props) => {
+	const [paintings, setPaintings] = useState([]);
+	useEffect(() => {
+		const getPaintings = async () => {
+			const voronoiPaintings = (
+				await axios.get(`/api/voronois/${props.voronoiId}`)
+			).data;
+			setPaintings(voronoiPaintings);
+		};
+		getPaintings();
+	}, []);
+	useEffect(() => {
+		if (paintings.length) {
+			const voronoiRender = chart.render(
+				"#single-voronoi",
+				720,
+				1080,
+				paintings
+			);
+			const interval = setInterval(() => voronoiRender.next(), 8);
+			return () => clearInterval(interval);
+		}
+	}, [paintings]);
+	return (
+		<div>
+			<canvas id="single-voronoi" width="1080" height="720"></canvas>
+		</div>
+	);
+};
