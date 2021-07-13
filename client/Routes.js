dae36bde402816ca80f109db0d13797a015732ab
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-13 15:28:16 -0400

    added route for women by women view

--- client/Routes.js
+++ client/Routes.js
@@ -1,12 +1,13 @@
-import React, {Component, Fragment} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
-import App from './App'
-import FeaturedPieces from './components/FeaturedPieces'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import WomenByWomen from './components/WomenByWomen'
-import WomenByMen from './components/WomenByMen'
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import App from "./App";
+import FeaturedPieces from "./components/FeaturedPieces";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import WomenByWomen from "./components/WomenByWomen";
+import WomenByMen from "./components/WomenByMen";
+import SingleVoronoi from "./components/SingleVoronoi";
 // import FeaturedPieces from './components/FeaturedPieces'
 // import VoronoiForm from './components/VoronoiForm'
 
@@ -14,22 +15,24 @@ import WomenByMen from './components/WomenByMen'
  * COMPONENT
  */
 class Routes extends Component {
-
-  render() {
-    return (
-      <div>
-        {/* <Route path="/" component={Navbar} /> */}
-        <Route exact path="/" component={Home} />
-      	<Route exact path="/main" component={App} />
-        <Route exact path="/womenbywomen" component={WomenByWomen} />
-        <Route exact path="/womenbymen" component={WomenByMen} />
-        {/* <Route exact path="/featured-pieces" component={FeaturedPieces} />
+	render() {
+		return (
+			<div>
+				{/* <Route path="/" component={Navbar} /> */}
+				<Route exact path="/" component={Home} />
+				<Route exact path="/main" component={App} />
+				<Route
+					exact
+					path="/womenbywomen"
+					render={(props) => <SingleVoronoi voronoiId={1} />}
+				/>
+				<Route exact path="/womenbymen" component={WomenByMen} />
+				{/* <Route exact path="/featured-pieces" component={FeaturedPieces} />
         <Route exact path="/voronoi-form" component={VoronoiForm} /> */}
-        <Route path='/all-art' component={FeaturedPieces} />
-
-      </div>
-    )
-  }
+				<Route path="/all-art" component={FeaturedPieces} />
+			</div>
+		);
+	}
 }
 
-export default withRouter(connect(null)(Routes))
+export default withRouter(connect(null)(Routes));
