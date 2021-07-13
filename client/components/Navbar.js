94ef61f197e42016c854fa3362667b0cb3fd9c30
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-07-13 09:07:01 -0400

    bug: removed unnecessary import

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
-import SideNav from 'react-simple-sidenav'
 
 const Navbar = (props) => {
 
