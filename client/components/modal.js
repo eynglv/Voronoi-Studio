af44dbee2b3eebf5e084d0abeb8d5b79611e7452
Elvy Yang <hyang6@wellesley.edu>
2021-07-14 03:14:00 +0800

    style: modal title/artist

--- client/components/modal.js
+++ client/components/modal.js
@@ -1,5 +1,4 @@
 const renderModal = (selector, artPiece) => {
-  console.log(artPiece);
   d3.select(selector).classed("open", true).classed("close", false);
 
   const modal = d3
@@ -12,23 +11,27 @@ const renderModal = (selector, artPiece) => {
         .append("div")
         .attr("class", "modalContent")
         .append("img")
+        .attr("class", "mx-auto")
         .attr("src", (data) => {
           return data.primaryImageSmall;
         });
-      enter.append("h3").text((data) => {
-        return data.title;
-      });
+      enter
+        .append("h3")
+        .attr("class", "text-center")
+        .text((data) => {
+          return data.title;
+        });
       enter.append("p").text((data) => {
         return `- ${data.artistDisplayName} -`;
       });
       return enter;
     });
 
-  d3.select(".closeBtn").on("click", closeModal);
-  d3.select("body").on("click", closeModal);
+  // d3.select(".closeBtn").on("click", closeModal);
+  // d3.select("body").on("click", closeModal);
 
-  function closeModal() {
-    d3.select(selector).classed("open", false).classed("close", true);
-  }
+  // function closeModal() {
+  //   d3.select(selector).classed("open", false).classed("close", true);
+  // }
 };
 export default renderModal;
