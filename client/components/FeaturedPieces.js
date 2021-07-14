6e469303be9257e60c70f0da6a4af8fbf6891a40
Phoebe Torchia <phoebetorchia@Phoebes-Air.fios-router.home>
2021-07-14 15:36:15 -0400

    style updates: featured pieces

--- client/components/FeaturedPieces.js
+++ client/components/FeaturedPieces.js
@@ -11,12 +11,12 @@ export default ()=>{
         getArt()
     },[])
     return(
-        <div className="mx-auto" style={{width: '90%'}}>
+        <div className="mx-auto mb-5" style={{width: '90%'}}>
         <h1 className="title display-2 mx-auto text-center mt-5">All Art Pieces</h1>
-        <div className='row row-cols-4 mx-auto mt-5'>
+        <div className='row row-cols-3 mx-auto mt-4'>
             {artPieces.map(painting=>(
                 <div className= "mb-5 mt-2 image-wrapper" key={painting.id} >
-                <img  className ="w-100 h-100" src={painting.primaryImageSmall} /> 
+                <img  className ="h-100 w-100 " src={painting.primaryImageSmall} /> 
                 <div className='mt-1 text-center caption'>{painting.title} by {painting.artistDisplayName ? painting.artistDisplayName : 'Unknown'}</div>
                 </div>
             ))}
