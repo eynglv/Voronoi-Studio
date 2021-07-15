3dcbad2fdd9806d46f5c59470d7894904c9b1586
Elvy Yang <hyang6@wellesley.edu>
2021-07-16 03:09:05 +0800

    merge conflicts

--- client/components/SingleVoronoi.js
+++ client/components/SingleVoronoi.js
@@ -4,35 +4,38 @@ import chart from "./pulsate";
 import axios from "axios";
 
 export default (props) => {
-	const [paintings, setPaintings] = useState([]);
-	useEffect(() => {
-		const getPaintings = async () => {
-			const voronoiPaintings = (
-				await axios.get(`/api/voronois/${props.voronoiId}`)
-			).data;
-			setPaintings(voronoiPaintings);
-		};
-		getPaintings();
-	}, []);
-	useEffect(() => {
-		if (paintings.length) {
-			const voronoiRender = chart.render(
-				"#single-voronoi",
-				600,
-				1000,
-				paintings
-			);
-			const interval = setInterval(() => voronoiRender.next(), 8);
-			return () => clearInterval(interval);
-		}
-	}, [paintings]);
-	return (
-			<div id="modal"></div>
-	<h1 className="text-center display-3 mt-3">{props.voronoiId}</h1>
-			<h3 className="text-center mb-2 mt-2">
-				Curated by {props.curator}
-			</h3>
-			<canvas id="single-voronoi" className="mx-auto mb-3" width="1000" height="600"></canvas>
-		</div>
-	);
+  const [paintings, setPaintings] = useState([]);
+  useEffect(() => {
+    const getPaintings = async () => {
+      const voronoiPaintings = (
+        await axios.get(`/api/voronois/${props.voronoiId}`)
+      ).data;
+      setPaintings(voronoiPaintings);
+    };
+    getPaintings();
+  }, []);
+  useEffect(() => {
+    if (paintings.length) {
+      const voronoiRender = chart.render(
+        "#single-voronoi",
+        600,
+        1000,
+        paintings
+      );
+      const interval = setInterval(() => voronoiRender.next(), 8);
+      return () => clearInterval(interval);
+    }
+  }, [paintings]);
+  return (
+    <div id="modal">
+      <h1 className="text-center display-3 mt-3">{props.voronoiId}</h1>
+      <h3 className="text-center mb-2 mt-2">Curated by {props.curator}</h3>
+      <canvas
+        id="single-voronoi"
+        className="mx-auto mb-3"
+        width="1000"
+        height="600"
+      ></canvas>
+    </div>
+  );
 };
