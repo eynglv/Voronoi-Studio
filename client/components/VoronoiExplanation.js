5cf7a76bd1584090ef3783d167a56ea728f1341d
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-07-16 12:59:37 -0400

    feat: added charts to voronoi explanation

---
+++ client/components/VoronoiExplanation.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/display-name */
+import chart from "./ExampleVoronoi";
+import React, { useEffect } from "react";
+
+export default () => {
+	useEffect(() => {
+		const exampleOne = chart.render("#example-one", 600, 1000);
+		const exampleTwo = chat.render("#example-two", 600, 100, 3);
+		const interval = setInterval(() => {
+			exampleOne.next();
+			exampleTwo.next();
+		}, 8);
+		return () => clearInterval(interval);
+	}, []);
+	return (
+		<div>
+			<canvas
+				id="example-one"
+				className="mx-auto mb-3"
+				width="1000"
+				height="600"
+			></canvas>
+			<canvas
+				id="example-two"
+				className="mx-auto mb-3"
+				width="1000"
+				height="600"
+			></canvas>
+		</div>
+	);
+};
