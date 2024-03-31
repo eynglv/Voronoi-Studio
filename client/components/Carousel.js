fd4f957d221694aafed0e0399a394ebde3c1718e
ElvyYang <eynglv@gmail.com>
2024-03-30 23:05:45 -0700

    carousel UI done

--- client/components/Carousel.js
+++ client/components/Carousel.js
@@ -1,18 +1,79 @@
 import React, { useState, useEffect } from "react";
 
-const CarouselItem = ({ imgSrc, title, caption }) => (
-  <div className=''>
-    <img src={imgSrc} className='md:shrink-0' alt />
-    <div className='' style={{ opacity: 0.8 }}>
-      <h5>{title}</h5>
-      <p className=''>{caption}</p>
+const CarouselItem = ({ carouselItem }) => {
+  const { imgSrc, title, caption, alt } = carouselItem;
+  return (
+    <div className='flex flex-col items-center'>
+      <img src={imgSrc} className='object-contain h-96' alt={alt} />
+      <div className='flex flex-col items-center' style={{ opacity: 0.8 }}>
+        <h5>{title}</h5>
+        <p className=''>{caption}</p>
+      </div>
     </div>
-  </div>
+  );
+};
+
+const SlideIndicator = ({ active }) => (
+  <div
+    className={`w-6 h-1 ${active ? "bg-primary-50" : "bg-primary-500"}`}
+  ></div>
+);
+
+const ChevronLeftIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-10 h-10 hover:text-primary-400'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='M15.75 19.5 8.25 12l7.5-7.5'
+    />
+  </svg>
+);
+
+const ChevronRightIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 24 24'
+    strokeWidth={1.5}
+    stroke='currentColor'
+    className='w-10 h-10 hover:text-primary-400'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      d='m8.25 4.5 7.5 7.5-7.5 7.5'
+    />
+  </svg>
 );
 
 const Carousel = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  useEffect(() => {}, []);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNextSlide = () => {
+    const nextIndex = (currentIndex + 1) % carouselItems.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePreviousSlide = () => {
+    const prevIndex =
+      (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    setCurrentIndex(prevIndex);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      handleNextSlide();
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, [currentIndex]);
 
   const carouselItems = [
     {
@@ -20,6 +81,7 @@ const Carousel = () => {
       title: "Fur Traders Descending the Missouri",
       caption:
         "This piece was originally titled French Trader & Half Breed Son",
+      alt: "painting depicting two people resting on a small boat",
     },
     {
       imgSrc:
@@ -27,6 +89,7 @@ const Carousel = () => {
       title: "Bodhissatva (12th Century)",
       caption:
         "This piece was originally titled French Trader & Half Breed Son",
+      alt: "bronze statue of a bodhissatva",
     },
     {
       imgSrc:
@@ -34,15 +97,35 @@ const Carousel = () => {
       title: "Landscapes (1814)",
       caption:
         "This piece was originally titled French Trader & Half Breed Son",
+      alt: "black and white sketch of a mountainous landscape",
     },
     {
       imgSrc:
         "https://collectionapi.metmuseum.org/api/collection/v1/iiif/313131/2185313/main-image",
-      title: "Fur Traders Descending the Missouri",
+      title: "Female Figure (12th-14th century)",
       caption: "Female Figure (12th-14th century)",
+      alt: "statue of female figure",
     },
   ];
-  return <div className='bg-primary-500 h-2/5'>hello</div>;
+
+  return (
+    <div className='flex flex-col justify-between h-3/5 w-full md:w-1/2 items-center'>
+      <div className='flex justify-between w-full'>
+        <button onClick={handlePreviousSlide}>
+          <ChevronLeftIcon />
+        </button>
+        <CarouselItem carouselItem={carouselItems[currentIndex]} />
+        <button onClick={handleNextSlide}>
+          <ChevronRightIcon />
+        </button>
+      </div>
+      <div className='flex justify-around w-2/6 md:w-3/12'>
+        {carouselItems.map((_, index) => (
+          <SlideIndicator active={index === currentIndex} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
