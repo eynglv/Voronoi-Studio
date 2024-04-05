f28dba637d2b72af08465b79f534597335d5afce
ElvyYang <eynglv@gmail.com>
2024-04-05 14:43:38 -0700

    try this?

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ var path = require("path");
 module.exports = {
   entry: ["./client/index.js"],
   output: {
-    path: __dirname,
-    filename: "./public/bundle.js",
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/",
   },
   devtool: "source-map",
   module: {
